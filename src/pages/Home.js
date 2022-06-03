07e3cb0398f447b3a16a29a99bfacfb6fa5fb7c0
JamesHughes <jhughes1790@gmail.com>
2022-06-03 13:56:34 -0400

    Home component updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,9 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Pedro</h2>
+        <h2> Hello, my name is James.</h2>
         <div className="prompt">
-          <p>A software developer with a passion for learning and creating.</p>
+          <p>I am a software developer with a passion for learning & helping others.</p>
           <LinkedInIcon />
           <EmailIcon />
           <GithubIcon />
@@ -22,20 +22,19 @@ function Home() {
           <li className="item">
             <h2> Front-End</h2>
             <span>
-              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+              ReactJS, Redux, HTML, CSS, React Native, NPM,
+             BootStrap, MaterialUI, Yarn,
             </span>
           </li>
           <li className="item">
             <h2>Back-End</h2>
             <span>
-              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+              ** Coming Soon! NodeJS, ExpressJS, MongoDB, Python
             </span>
           </li>
           <li className="item">
             <h2>Languages</h2>
-            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+            <span>JavaScript</span>
           </li>
         </ol>
       </div>
