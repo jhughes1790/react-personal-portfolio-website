dfbeaeb026e43179ff01a9619697cbd6b32be3b0
JamesHughes <jhughes1790@gmail.com>
2022-06-04 09:52:25 -0400

    social media links added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,32 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
 import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Home.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hello, my name is James.</h2>
+        <h2> Hello, my name is Jamie.</h2>
         <div className="prompt">
           <p>I am a software developer with a passion for learning & helping others.</p>
-          <LinkedInIcon />
-          <EmailIcon />
-          <GithubIcon />
+          <a href="https://www.linkedin.com/in/james-hughes-24006713b/" target="_blank" rel="noreferrer">
+            <LinkedInIcon />
+          </a>
+          <a href="mailto:jhughes1790@gmail.com?subject=Hello Jamie!">
+            <EmailIcon />
+          </a>
+              
+          
+          <a href="https://github.com/jhughes1790" target="_blank" rel="noreferrer">
+              <GithubIcon />
+          </a>
         </div>
       </div>
       <div className="skills">
